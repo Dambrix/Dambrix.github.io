75ba8e14df44d736fda4228951b15aa002ae3daa
Davide <davidedambrix@gmail.com>
2021-03-14 19:02:37 +0100

    Almost the entire folder

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+	import "./style.css"
+</script>
+
+<div class="gif-container">
+	<img src="https://media.giphy.com/media/ocuQpTqeFlDOP4fFJI/giphy-downsized.gif" alt="Under construction"
+		 id="gif-under-construction">
+</div>
